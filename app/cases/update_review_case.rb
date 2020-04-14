b9de9456d1748a4870ef6bb23785ca3632239fc0
isaacmeedinaa <isaac.meedinaa@gmail.com>
2020-04-14 11:49:29 -0500

    finished update case

--- app/cases/update_review_case.rb
+++ app/cases/update_review_case.rb
@@ -9,12 +9,12 @@ def update_review(user)
     puts "\n"
     puts "Please provide a new review text:"
     puts "\n"
-    new_review_text = gets.chomp
+    updated_review_text = gets.chomp
 
     puts "\n"
     puts "Please provide a new review rating:"
     puts "\n"
-    new_review_rating = gets.chomp
+    updated_review_rating = gets.chomp
 
     selected_review = list[num.to_i - 1]
 
@@ -22,7 +22,7 @@ def update_review(user)
     puts "Are you sure?"
     puts "\n"
     if gets.chomp == "Yes"
-        selected_review.update(review_text: new_review_text, rating: new_review_rating)
+        selected_review.update(review_text: updated_review_text, rating: updated_review_rating)
         puts "Your review has been updated!"
     else
         puts "Review has not been updated."
