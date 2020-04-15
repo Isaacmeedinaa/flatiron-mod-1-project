3e57824a96fb8072766f232bf2d33426733ae3e3
Shau Yin Luk <ivan.luk@icloud.com>
2020-04-15 09:54:51 -0500

    clean up

--- app/cases/list_all_reviews_case.rb
+++ app/cases/list_all_reviews_case.rb
@@ -1,11 +1,14 @@
 def list_all_reviews(user)
-  puts "Here are all your reviews!"
-  puts "\n"
   review_list = user.reviews.order(created_at: :desc)
-  review_list.each_with_index do |item, index|
-    puts "#{index + 1}. #{item.restaurant.name} (#{item.restaurant.restaurant_food_type.name}) - Rating: #{item.rating} on #{item.created_at.strftime("%F")}"
-    puts "     Comments: '#{item.review_text}'"
+  if review_list.length > 0
+    review_list.each_with_index do |item, index|
+      puts "#{index + 1}. #{item.restaurant.name} (#{item.restaurant.restaurant_food_type.name}) - Rating: #{item.rating} on #{item.created_at.strftime("%F")}"
+      puts "     Comments: '#{item.review_text}'"
+    end
+    puts "Here are all your reviews!"
+  else 
+    puts "You have no reviews."  
   end
-  # 
+  puts "\n"
   review_list
 end
