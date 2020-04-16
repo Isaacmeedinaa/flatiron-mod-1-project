03b3af0d2ac8a3751ea4df56f9d5a956a2a267ad
Shau Yin Luk <ivan.luk@icloud.com>
2020-04-16 15:09:36 -0500

    mark favorite with emoji

--- app/cases/utilities.rb
+++ app/cases/utilities.rb
@@ -1,10 +1,16 @@
 # this method is a utility to produce a formated list for use in a emnum select
 # it takes in a list and the column needs to be extracted into the TTY response
-def get_enum_list(some_list, column)
+def get_enum_list(some_list, column, fav=nil)
   num_list = []
   some_list.each_with_index do |value, index|
       liste = []
-      liste << value[column]
+      mark = value[column]
+      if fav
+        if index == fav
+          mark = mark + " 👍"
+        end
+      end
+      liste << mark
       liste << index + 1
       num_list << liste
   end
