cf9f54ccff1a18117eebb6b075a5f0caa10f3729
Shau Yin Luk <ivan.luk@icloud.com>
2020-04-16 18:12:57 -0500

    add restatuant review list

--- app/cases/list_all_restaurants_case.rb
+++ app/cases/list_all_restaurants_case.rb
@@ -43,7 +43,7 @@ def list_all_restaurants(user)
                 puts "  #{index + 1}) '#{item.review_text}' - Rating: #{item.rating} on #{item.created_at.strftime("%F")}"
             end
         else     
-            puts "No reviews for this restaurant."
+            puts "No review for this restaurant."
         end
     end
     puts "\n"
